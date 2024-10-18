091ee546f9cc4cfd3be35d124bb0ae6aa404ad32
Satori527 <anubhavsharma527@gmail.com>
2024-10-18 11:55:42 +0530

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8000
+  },
+  build: {
+  rollupOptions: {
+    external: ['@mui/icons-material/Eventnote']
+    }
+  }
 })
+
+
+// export default {
+//   build: {
+//     rollupOptions: {
+//       external: ['@mui/icons-material/Eventnote']
+//     }
+//   }
+// }
