fc344105bce21a4e7a43955ef01d60a29911a7a8
Satori527 <anubhavsharma527@gmail.com>
2024-10-18 13:53:55 +0530

    rollup

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,11 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-  rollupOptions: {
-    external: ['/@mui/icons-material/Eventnote']
-    }
-  }
+  // build: {
+  // // rollupOptions: {
+  // //   external: ['/@mui/icons-material/Eventnote']
+  // //   }
+  // // }
 })
 
 
