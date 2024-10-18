0514fc3f297e5111c3c3d6c230a9147798e342d6
Satori527 <anubhavsharma527@gmail.com>
2024-10-18 13:31:31 +0530

    vercel

--- Server/src/app.js
+++ Server/src/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true
 }))
 
@@ -13,6 +14,7 @@ app.use(express.json({limit: "32kb"}))
 app.use(express.urlencoded({extended:true, limit:"32kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
 
 
 //routes
